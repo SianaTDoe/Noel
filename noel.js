4594ce9d32765631a04c100b1fe1b79cc1f6057b
Linaoshana <sianadoe@gmail.com>
2020-12-10 20:02:31 +0100

    img

--- noel.js
+++ noel.js
@@ -4,9 +4,26 @@ let result1 = document.getElementById("result1");
 submit1.addEventListener("click", (e) => {
     e.preventDefault();
 
-    result1.textContent =("Salut toi")
+    result1.innerHTML ="<img src= img_noel/jour1.gif>"
 
 });
+
+
+let submit2 = document.getElementById("submit2");
+let result2 = document.getElementById("result2");
+
+submit2.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    result2.innerHTML ="<img src= img_noel/jour2.png>"
+
+});
+
+
+
+
+
+
 
 
 
@@ -17,6 +34,6 @@ let result25= document.getElementById("result25");
 submit25.addEventListener("click", (e) => {
     e.preventDefault();
 
-    result25.innerHTML =(`Noël Joyeux ! <video src= https://www.youtube.com/watch?v=g-OF7KGyDis width="375" height="280" controls></video>`)
+    result25.innerHTML =(`<video src= img_noel/jour25.mp4 width="500" height="280" controls></video>`)
 
 });
